03d4f630b6819a1002dc542ad09c481e72402688
Nicro <60424564+nicro296@users.noreply.github.com>
2022-06-02 03:57:22 +0900

    Update content-log.js

--- js/content-log.js
+++ js/content-log.js
@@ -2,7 +2,7 @@
 let div_app = document.getElementById('app');
 div_app.style.display = 'none';
 
-chrome.storage.local.get('bl_hide_rate',function(result){
+chrome.storage.local.get(['bl_hide_rate','bl_hide_opponent_rate'],function(result){
     if(result.bl_hide_rate != null){
         if(result.bl_hide_rate){
             if(div_app.children!=null && div_app.children.length >1){
@@ -15,11 +15,24 @@ chrome.storage.local.get('bl_hide_rate',function(result){
                                         if(div_app.children[1].children[0].children[0].children[0].children[0].children[1].children!=null
                                             && div_app.children[1].children[0].children[0].children[0].children[0].children[1].children.length>1){
                                                 
-            if(div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].textContent == 'あなた'){
-                div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[0].textContent = '**** pt';
-            }else if(div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[1].children[0].textContent == 'あなた'){
-                div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[1].children[0].textContent = '**** pt';
-            }
+                                                if(result.bl_hide_opponent_rate != null){
+                                                    if(result.bl_hide_opponent_rate){
+                                                        div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[0].textContent = '**** pt';
+                                                        div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[1].children[0].textContent = '**** pt';
+                                                        div_app.style.display = '';
+                                                    }else{
+                                                        if(div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].textContent == 'あなた'){
+                                                            div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[0].children[0].textContent = '**** pt';
+                                                            div_app.style.display = '';
+                                                        }else if(div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[0].children[1].children[0].textContent == 'あなた'){
+                                                            div_app.children[1].children[0].children[0].children[0].children[0].children[1].children[1].children[1].children[0].textContent = '**** pt';
+                                                            div_app.style.display = '';
+                                                        }
+                                                    }
+                                                }else{
+                                                    chrome.storage.local.set({'bl_hide_opponent_rate':false},function(){});
+                                                }
+                                            
                                         }
                                     }
                             }
@@ -30,6 +43,7 @@ chrome.storage.local.get('bl_hide_rate',function(result){
         }else{
         }
     }else{
+        chrome.storage.local.set({'bl_hide_rate':false},function(){});
     }
 });
 div_app.style.display = '';
