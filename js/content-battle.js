236c495e5ac8335ac033805e934c45c5a401d4f7
Nicro <nicro296@gmail.com>
2022-06-08 05:59:52 +0900

    読み込み処理の修正

--- js/content-battle.js
+++ js/content-battle.js
@@ -25,6 +25,8 @@ function check_rate(target_rate){
     function jsLoaded() {
         let iframe1 = document.getElementById("reading-rate");
         if (iframe1 != null 
+            && iframe1.contentWindow != null
+            && iframe1.contentWindow.document != null
             && iframe1.contentWindow.document.getElementById('app') != null
             && iframe1.contentWindow.document.getElementById('app').children != null
             && iframe1.contentWindow.document.getElementById('app').children[1].children != null
