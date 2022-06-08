c41e9abbe053e8c97ed7827094eacfe209964be9
Nicro <nicro296@gmail.com>
2022-06-08 17:01:02 +0900

    alertからbody内表記に変更

--- js/content-battle.js
+++ js/content-battle.js
@@ -52,6 +52,50 @@ function set_mypage(){
     document.body.appendChild(div_insert);
 }
 
+function display_alert(){
+    let div_panel_default = document.createElement("div");
+    div_panel_default.classList.add("panel");
+    div_panel_default.classList.add("panel-default");
+    let div_panel_body = document.createElement("div");
+    div_panel_body.classList.add("panel-body");
+    let p_text_container = document.createElement("p");
+    let span_alert = document.createElement("span");
+    span_alert.classList.add("color-red");
+    span_alert.style = "color:red;font-size:150%;";
+    span_alert.append(document.createTextNode("目標レートに到達しました！レート値非表示を解除にしますか？"));
+    let div_input_container = document.createElement("div");
+    div_input_container.style.textAlign = "right";
+    let input_yes = document.createElement("input");
+    input_yes.type = "button";
+    input_yes.style = "margin:0px 10px";
+    input_yes.value = "はい";
+    let input_no = document.createElement("input");
+    input_no.type = "button";
+    input_no.style = "margin:0px 10px";
+    input_no.value = "いいえ";
+    p_text_container.append(span_alert);
+    div_input_container.append(input_yes);
+    div_input_container.append(input_no);
+    div_panel_body.append(p_text_container);
+    div_panel_body.append(div_input_container);
+    div_panel_default.append(div_panel_body);
+    div_app.children[1].children[0].children[0].children[0].insertBefore(div_panel_default,div_app.children[1].children[0].children[0].children[0].firstChild);
+
+    input_yes.addEventListener('click',()=>{
+        span_alert.textContent = '';
+        div_input_container.removeChild(input_yes);
+        div_input_container.removeChild(input_no);
+        span_alert.append(document.createTextNode("目標レートに到達しました！レート値非表示を解除します。"));
+        chrome.storage.local.set({'bl_hide_rate':false},function(){});
+    });
+    input_no.addEventListener('click',()=>{
+        span_alert.textContent = '';
+        div_input_container.removeChild(input_yes);
+        div_input_container.removeChild(input_no);
+        span_alert.append(document.createTextNode("目標レートに到達しました！(レート値非表示継続中)"));
+    });
+}
+
 /**
  * 目標レート値を超えているかの判別と超えている時の処理
  * @param {int} target_rate 目標レート値 
@@ -66,8 +110,7 @@ function check_rate(target_rate){
             if(iframe1.contentWindow.document.getElementById('app').children[1].children[0].children[0].children[0].children[8].children[0].textContent.indexOf('レーティング')>-1){
                 rate = iframe1.contentWindow.document.getElementById('app').children[1].children[0].children[0].children[0].children[8].children[0].children[1].textContent.substring(0,4).trim();
                 if(rate >= target_rate){
-                    window.alert('目標レートに到達しました。レート値の非表示を解除します');
-                    chrome.storage.local.set({'bl_hide_rate':false},function(){});
+                    display_alert();
                 }
             }
         }
@@ -108,7 +151,7 @@ function main(){
                         }else{//対戦ページ以外の時
                             set_mypage();
                             if(result.target_rate != null){
-                                document.addEventListener("load", check_rate(result.target_rate), false);
+                                window.addEventListener("load", check_rate(result.target_rate), false);
                             }else{
                                 chrome.storage.local.set({'target_rate':0},function(){});
                             }
