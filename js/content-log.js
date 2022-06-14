5db52ba3ab8cf9139f04b7bbbecee3cb8bfed0b1
Nicro <nicro296@gmail.com>
2022-06-15 01:28:12 +0900

    戦績ページのjsのローテ,2Pick統合

--- js/content-log.js
+++ js/content-log.js
@@ -1,6 +1,26 @@
-
+const url_rotation = 'https://g-ratings.info/log/';
+const url_2Pick = 'https://2pick.g-ratings.info/log/';
 let div_app = document.getElementById('app');
-div_app.style.display = 'none';
+
+const bl_hide_rate_rotation = 'bl_hide_rate_rotation';
+const bl_hide_opponent_rate_rotation = 'bl_hide_opponent_rate_rotation';
+const bl_hide_rate_2Pick = 'bl_hide_rate_2Pick';
+const bl_hide_opponent_rate_2Pick = 'bl_hide_opponent_rate_2Pick';
+
+let bl_hide_rate = bl_hide_rate_rotation;
+let bl_hide_opponent_rate = bl_hide_opponent_rate_rotation;
+
+if(location.href.indexOf(url_rotation) >-1){
+    bl_hide_rate = bl_hide_rate_rotation;
+    bl_hide_opponent_rate = bl_hide_opponent_rate_rotation;
+    console.log('rotation');
+}else if(location.href.indexOf(url_2Pick) >-1){
+    bl_hide_rate = bl_hide_rate_2Pick;
+    bl_hide_opponent_rate = bl_hide_opponent_rate_2Pick;
+    console.log('2Pick');
+}
+
+
 
 function check_load(){
     if(div_app.children != null
@@ -18,48 +38,47 @@ function check_load(){
 }
 
 function main(){
-    chrome.storage.local.get(['bl_hide_rate_rotation','bl_hide_opponent_rate_rotation'],function(result){
-        if(result.bl_hide_rate_rotation != null){
-            if(result.bl_hide_rate_rotation){
-                let jsInitCheckTimer = setInterval(jsLoaded, 100);
+    chrome.storage.local.get([bl_hide_rate,bl_hide_opponent_rate],function(result){
+        if(result[bl_hide_rate] != null){
+            if(result[bl_hide_rate]){
+                let count = 1;
+                let jsInitCheckTimer = setInterval(jsLoaded, 100*count);
                 function jsLoaded() {
+                    count++;
                     if(check_load()){
                         clearInterval(jsInitCheckTimer);
                         let div5 = div_app.children[1].children[0].children[0].children[0].children[0].children[1];
                         if(div5.children!=null && div5.children.length>1){     
     
-                            if(result.bl_hide_opponent_rate_rotation != null){
-                                if(result.bl_hide_opponent_rate_rotation){
+                            if(result[bl_hide_opponent_rate] != null){
+                                if(result[bl_hide_opponent_rate]){
                                     div5.children[1].children[0].children[0].textContent = '**** pt';
                                     div5.children[1].children[1].children[0].textContent = '**** pt';
-                                    div_app.style.display = '';
                                 }else{
                                     if(div5.children[0].children[0].children[0].textContent == 'あなた'){
                                         div5.children[1].children[0].children[0].textContent = '**** pt';
-                                        div_app.style.display = '';
                                     }else if(div5.children[0].children[1].children[0].textContent == 'あなた'){
                                         div5.children[1].children[1].children[0].textContent = '**** pt';
-                                        div_app.style.display = '';
                                     }
                                 }
                             }else{
-                                chrome.storage.local.set({'bl_hide_opponent_rate_rotation':false},function(){});
+                                chrome.storage.local.set({bl_hide_opponent_rate:false},function(){});
                             }
             
                         }
+                    }
+                    if(count > 20){
+                        clearInterval(jsInitCheckTimer);
+                        console.log('errorlog[countover]');
                     }
                 }
             }else{
             }
         }else{
-            chrome.storage.local.set({'bl_hide_rate_rotation':false},function(){});
+            chrome.storage.local.set({bl_hide_rate:false},function(){});
         }
     });
 }
 
-function final(){
-    div_app.style.display = '';
-}
 
 main();
-final();
