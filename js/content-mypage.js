7a4241e3c8948fa75146d1146fab8c34dcdb1270
Nicro <nicro296@gmail.com>
2022-06-15 02:43:01 +0900

    マイページの到達時非表示削除を[はい、いいえ]クリックに変更

--- js/content-mypage.js
+++ js/content-mypage.js
@@ -9,6 +9,7 @@ const target_rate_rotation = 'target_rate_rotation';
 const child4_2Pick = 4;
 const bl_hide_rate_2Pick = 'bl_hide_rate_2Pick';
 const target_rate_2Pick = 'target_rate_2Pick';
+let alert_display = false;
 let rate='';
 let child4 = child4_rotation;
 let bl_hide_rate = bl_hide_rate_rotation;
@@ -32,6 +33,59 @@ if(location.href == url_rotation){
  * @type {boolean} アラート重複回避用
  */
 let set_alert = false;
+
+function display_alert(){
+    if(alert_display){
+        div_app.children[1].children[0].children[0].children[0].removeChild(div_app.children[1].children[0].children[0].children[0].children[0]);
+    }else{
+        child4++;
+    }
+    
+    alert_display = true;
+    let div_panel_default = document.createElement("div");
+    div_panel_default.classList.add("panel");
+    div_panel_default.classList.add("panel-default");
+    let div_panel_body = document.createElement("div");
+    div_panel_body.classList.add("panel-body");
+    let p_text_container = document.createElement("p");
+    let span_alert = document.createElement("span");
+    span_alert.classList.add("color-red");
+    span_alert.style = "color:red;font-size:150%;";
+    span_alert.append(document.createTextNode("目標レートに到達しました！レート値非表示を解除にしますか？"));
+    let div_input_container = document.createElement("div");
+    div_input_container.style.textAlign = "right";
+    let input_yes = document.createElement("input");
+    input_yes.type = "button";
+    input_yes.style = "margin:0px 10px";
+    input_yes.value = "はい";
+    let input_no = document.createElement("input");
+    input_no.type = "button";
+    input_no.style = "margin:0px 10px";
+    input_no.value = "いいえ";
+    p_text_container.append(span_alert);
+    div_input_container.append(input_yes);
+    div_input_container.append(input_no);
+    div_panel_body.append(p_text_container);
+    div_panel_body.append(div_input_container);
+    div_panel_default.append(div_panel_body);
+    div_app.children[1].children[0].children[0].children[0].insertBefore(div_panel_default,div_app.children[1].children[0].children[0].children[0].firstChild);
+
+    input_yes.addEventListener('click',()=>{
+        span_alert.textContent = '';
+        div_input_container.removeChild(input_yes);
+        div_input_container.removeChild(input_no);
+        span_alert.append(document.createTextNode("目標レートに到達しました！レート値非表示を解除します。"));
+        const bl_hide_rate_temp = {[bl_hide_rate] : false};
+        chrome.storage.local.set(('bl_hide_rate_temp',bl_hide_rate_temp),function(){});
+        location.reload();
+    });
+    input_no.addEventListener('click',()=>{
+        span_alert.textContent = '';
+        div_input_container.removeChild(input_yes);
+        div_input_container.removeChild(input_no);
+        span_alert.append(document.createTextNode("目標レートに到達しました！(レート値非表示継続中)"));
+    });
+}
 
 function check_load(){
     if(div_app.children != null
@@ -71,12 +125,13 @@ function check_rate(target_rate){
                 let word = div3.children[child4].children[0].children[1].textContent;
                 rate = word.substring(0,word.indexOf('[RD')).trim();
                 word = word.substring(word.indexOf('[RD'));
-                if(parseFloat(target_rate) <= parseFloat(rate)){
-                    chrome.storage.local.set({bl_hide_rate:false},function(){});
-                    window.alert('目標レートに到達しました。レート値の非表示を解除します');
+                word = '**** '+word;
+                div3.children[child4].children[0].children[1].textContent = word;
+                if(rate == '****'){
+                }else if(parseFloat(target_rate) <= parseFloat(rate)){
+                    window.alert('目標レートに到達しました。');
+                    display_alert();
                 }else{
-                    word = '**** '+word;
-                    div3.children[child4].children[0].children[1].textContent = word;
                 }
             }
         }
@@ -99,12 +154,14 @@ function hide_rate(){
                         check_rate(result[target_rate]);
                     }
                 }else{
-                    chrome.storage.local.set({target_rate:0},function(){});
-                }           
+                    const taret_rate_temp = {[target_rate]:0};
+                    chrome.storage.local.set(('taret_rate_temp',taret_rate_temp),function(){});
+                }
             }else{
             }
         }else{
-            chrome.storage.local.set({bl_hide_rate:false},function(){});
+            const bl_hide_rate_temp = {[bl_hide_rate]:false};
+            chrome.storage.local.set(('bl_hide_rate_temp',bl_hide_rate_temp),function(){});
         }
     });
 }
